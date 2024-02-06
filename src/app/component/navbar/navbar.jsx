79a2925f5779c7fea1fb5a20d5bc081c437502ae
mankinde23 <damilolamakinde493@gmail.com>
2024-02-06 16:54:12 +0100

    finished the first prototype for the navbar

--- src/app/component/navbar/navbar.jsx
+++ src/app/component/navbar/navbar.jsx
@@ -1,19 +1,35 @@
 import React from "react";
 import Logo from "../logo/logo";
-import { FiSearch } from "react-icons/fi";
+
+import { IoMdCart } from "react-icons/io";
+import { FiSearch, FiLogOut } from "react-icons/fi";
+import { AiOutlineUser } from "react-icons/ai";
 
 export default function Navbar() {
   return (
-    <div className="h-[5rem] bg-bodyColor top-0 sticky z-50">
+    <div className="h-[5rem] bg-bodyColor top-0 sticky z-50 p-[3rem]">
       <div className="h-full flex items-center md:gap-x-5 justify-between md:justify-start">
         <Logo />
-        <div className=" w-[27rem] bg-white hidden md:flex items-center gap-x-1 border-[1px] border-lightText/50 rounded-full px-4 py-1.5 focus-within:border-orange-600 group">
+        <div className=" w-[40rem] bg-white hidden md:flex items-center gap-x-1 border-[1px] border-lightText/50 rounded-full px-4 py-1.5 focus-within:border-orange-600 group">
           <FiSearch className="text-gray-500 group-focus-within:text-darkText duration-200" />
           <input
             type="text"
             placeholder="Search for products"
             className="placeholder:text-sm flex-1 outline-none"
           />
+        </div>
+        {/* login */}
+        <div className=" bg-bgLight text-gray-500 flex items-center justify-center p-1.5 rounded-full  hover:bg-white border-[2px] duration-500 ">
+          <AiOutlineUser className="text-2xl" />
+          <p className="text-sm font-semibold">Login/Register</p>
+        </div>
+        {/* cart */}
+        <div className="bg-black hover:bg-slate-950 rounded-full text-slate-100 hover:text-white flex items-center justify-center gap-x-1 px-3 py-1.5 border-[1px] border-black hover:border-orange-600 duration-200 relative">
+          <IoMdCart className="text-xl" />
+          <p className="text-sm font-semibold">$0.00</p>
+          <span className="bg-white text-orange-600 rounded-full text-xs font-semibold absolute -right-2 -top-1 w-5 h-5 flex items-center justify-center shadow-xl shadow-black">
+            0
+          </span>
         </div>
       </div>
     </div>
