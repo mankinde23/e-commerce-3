28f35b507fcef37c6c59a4bcde23b9af0737a4ca
mankinde23 <damilolamakinde493@gmail.com>
2024-02-07 18:08:47 +0100

    added the session provider component to the layout over the main container

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -1,4 +1,5 @@
 import Navbar from "./component/navbar/navbar";
+import AuthProvider from "./component/sessionprovider/sessionprovider";
 import "./globals.css";
 import { Inter } from "next/font/google";
 
@@ -13,10 +14,12 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body className={inter.className}>
-        <div className="">
-          <Navbar />
-          {children}
-        </div>
+        <AuthProvider>
+          <div className="">
+            <Navbar />
+            {children}
+          </div>
+        </AuthProvider>
       </body>
     </html>
   );
