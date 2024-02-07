b9ce409c17fd2a958927d5115f5f18a0f73821d6
mankinde23 <damilolamakinde493@gmail.com>
2024-02-07 18:05:59 +0100

    implemented the next auth folder structure to help in loging in with any provider

--- src/app/api/auth/[...nextauth]/route.js
+++ src/app/api/auth/[...nextauth]/route.js
@@ -1,5 +1,6 @@
 import NextAuth from "next-auth/next";
+import { options } from "./option";
 
-const handler = NextAuth();
+const handler = NextAuth(options);
 
 export { handler as GET, handler as POST };
