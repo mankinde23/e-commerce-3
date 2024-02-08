91890b2d29353285229508c6943180ad4f4ec737
mankinde23 <damilolamakinde493@gmail.com>
2024-02-08 07:50:58 +0100

    added a new component to the home component

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,12 +1,11 @@
-import Image from "next/image";
+
+
+import Banner from "./component/banner/Banner";
 
 export default function Home() {
   return (
     <main className="flex h-screen flex-col items-center justify-between p-24 bg-black text-white w-full">
-      Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempore
-      consequuntur repellat inventore magnam assumenda aliquam blanditiis
-      impedit reiciendis, enim sit vel tempora perferendis, nulla consequatur
-      nam placeat ducimus sed ipsa.
+      <Banner />
     </main>
   );
 }
