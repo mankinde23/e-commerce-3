73fa739a9312f38957b49fd7c869a37e77ba5a49
mankinde23 <damilolamakinde493@gmail.com>
2024-02-09 17:49:40 +0100

    commented out a component =

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -5,7 +5,7 @@ import Banner from "./component/banner/Banner";
 export default function Home() {
   return (
     <main className="flex h-screen flex-col items-center justify-between p-24 bg-black text-white w-full">
-      <Banner />
+      {/* <Banner /> */}
     </main>
   );
 }
