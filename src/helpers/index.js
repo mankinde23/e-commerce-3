5475d61d9beb385d2a98c624cfeb0250518f7c50
mankinde23 <damilolamakinde493@gmail.com>
2024-02-16 13:03:07 +0100

    added a new file to store the api data and call it when needed

---
+++ src/helpers/index.js
@@ -0,0 +1,10 @@
+export const getProducts = async () => {
+  const res = await fetch(
+    "https://fakestoreapiserver.reactbd.com/amazonproducts"
+  );
+  if (!res.ok) {
+    throw new Error("Failed to fetch products");
+  }
+  return res.json();
+  console.log(res);
+};
