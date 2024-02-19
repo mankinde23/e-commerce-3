be5cdc4bf9896402ee8f9d6e25925372c9681305
mankinde23 <damilolamakinde493@gmail.com>
2024-02-19 21:18:57 +0100

    commited out to the code and fetched the data in a better way

--- src/helpers/index.js
+++ src/helpers/index.js
@@ -1,10 +1,26 @@
-export const getProducts = async () => {
-  const res = await fetch(
-    "https://fakestoreapiserver.reactbd.com/amazonproducts"
-  );
-  if (!res.ok) {
-    throw new Error("Failed to fetch products");
-  }
-  return res.json();
+// export const getProducts = async () => {
+//   const res = await fetch(
+//     "https://fakestoreapiserver.reactbd.com/amazonproducts"
+//   );
+//   if (!res.ok) {
+//     throw new Error("Failed to fetch products");
+//   }
+//   return res.json();
+// };
+// // helpers/products.js
 
-};
+// export async function getProductsById(id) {
+//   try {
+//     const response = await fetch(
+//       `https://fakestoreapiserver.reactbd.com/amazonproducts/${id}`
+//     );
+//     if (!response.ok) {
+//       throw new Error("Failed to fetch product");
+//     }
+//     const product = await response.json();
+//     return product;
+//   } catch (error) {
+//     console.error("Error fetching product:", error);
+//     throw error; // Rethrow the error to handle it in the calling component
+//   }
+// }
