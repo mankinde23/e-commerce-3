5a4c43251bb774f60fc854e4603d6bad710f138c
mankinde23 <damilolamakinde493@gmail.com>
2024-02-22 10:16:45 +0100

    performed the logic for a shop cart which are adding,decreasing,restting and delete and will send the action to where i want to use it using a dispatch

--- src/redux/shoppingSlice.js
+++ src/redux/shoppingSlice.js
@@ -9,7 +9,39 @@ export const shoppingSlice = createSlice({
   initialState,
   reducers: {
     addToCart: (state, action) => {
+      const existingProduct = state.productData.find(
+        (item) => item.id === action.payload.id
+      );
+      if (existingProduct) {
+        existingProduct.quantity += action.payload.quantity;
+      } else {
+        state.productData.push(action.payload);
+      }
       state.productData = action.payload;
+    },
+    increaseQuantity: (state, action) => {
+      const existingProduct = state.productData.find(
+        (item) => item.id === action.payload.id
+      );
+      existingProduct && existingProduct.quantity++;
+    },
+    decreaseQuantity: (state, action) => {
+      const existingProduct = state.productData.find(
+        (item) => item.id === action.payload.id
+      );
+      if (existingProduct?.quantity === 1) {
+        existingProduct.quantity === 1;
+      } else {
+        existingProduct && existingProduct.quantity--;
+      }
+    },
+    deleteProduct: (state, action) => {
+      state.productData = state.productData.filter(
+        (item) => item.id !== action.payload
+      );
+    },
+    resetCart: (state) => {
+      state.productData = [];
     },
   },
 });
