c196984eafea218741a91da1554365b7f933a2f3
mankinde23 <damilolamakinde493@gmail.com>
2024-02-23 01:13:15 +0100

    perfomed the logic that adds the total  price of the product added to cart in the navbar

--- src/app/component/navbar/navbar.jsx
+++ src/app/component/navbar/navbar.jsx
@@ -1,14 +1,25 @@
 "use client";
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Logo from "../logo/logo";
 
 import { IoMdCart } from "react-icons/io";
 import { FiSearch, FiLogOut } from "react-icons/fi";
 import { AiOutlineUser } from "react-icons/ai";
 import { useSession, signIn, signOut } from "next-auth/react";
+import { useSelector } from "react-redux";
 
 export default function Navbar() {
   const { data: session } = useSession();
+  const { productData, orderData } = useSelector((state) => state.shopping);
+  const [totalAmt, setTotalAmt] = useState(0);
+  useEffect(() => {
+    let amt = 0;
+    productData.map((item) => {
+      amt += item.price * item.id;
+      return;
+    });
+    setTotalAmt(amt);
+  }, [productData]);
   return (
     <div className="h-[5rem] bg-bodyColor top-0 sticky z-50 p-[3rem]">
       <div className="h-full flex items-center md:gap-x-5 justify-between md:justify-start">
@@ -34,9 +45,9 @@ export default function Navbar() {
         {/* cart */}
         <div className="bg-black hover:bg-slate-950 rounded-full text-slate-100 hover:text-white flex items-center justify-center gap-x-1 px-3 py-1.5 border-[1px] border-black hover:border-orange-600 duration-200 relative">
           <IoMdCart className="text-xl" />
-          <p className="text-sm font-semibold">$0.00</p>
+          <p className="text-sm font-semibold">{totalAmt ? totalAmt : 0} </p>
           <span className="bg-white text-orange-600 rounded-full text-xs font-semibold absolute -right-2 -top-1 w-5 h-5 flex items-center justify-center shadow-xl shadow-black">
-            0
+            {productData ? productData?.length : 0}
           </span>
         </div>
         {/* user Image */}
